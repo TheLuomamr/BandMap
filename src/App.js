ea10613fca7335e479b08e2352067c55b4841027
sam w <42546742+s07w@users.noreply.github.com>
2019-03-28 20:58:51 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import SignUp from "./Components/SignUp/SignUp";
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SignUp />
+      </div>
+    );
+  }
+}
+
+export default App;
