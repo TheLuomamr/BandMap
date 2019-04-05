10228ddc236d4298d32a338e0f1fc00f023f7ed5
samjw <wagnerjsamuel@gmail.com>
2019-04-05 19:29:00 -0400

    added model for band schema, FINALLY FIXED ABOUT SECTION CSS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, /*Route, Switch*/ } from "react-router-dom"
-// import Nav from "./Components/Nav/Nav"
-//import Container from "./Components/Container/Container"
-//import Wrapper from "./Components/Wrapper/Wrapper"
-// import SignUp from "./Components/SignUp/SignUp";
-// import About from "./pages/About";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Nav from "./Components/Nav/Nav"
+// import Container from "./Components/Container/Container"
+// import Wrapper from "./Components/Wrapper/Wrapper"
+import SignUp from "./Components/SignUp/SignUp";
+import About from "./pages/About";
 import Login from "./Components/login/login";
 import './App.css';
-// import MyMapComponent from './Components/MyMapComponent/MyMapComponent';
+import MyMapComponent from './Components/MyMapComponent/MyMapComponent';
 
 const App = () => 
   <Router>
     <div>
         <Login/>
-        {/* <SignUp /> */}
+        {/* <SignUp />  */}
         {/* <Nav />
         <Switch>
           <Route exact path="/" component={SignUp}/>
           <Route exact path="/about" component={About}/>
           <Route exact path="/home" component={MyMapComponent}/>
-        </Switch> */}
-        {/* <MyMapComponent /> */}
+        </Switch>
+        <MyMapComponent /> */}
     </div>
   </Router>
 
